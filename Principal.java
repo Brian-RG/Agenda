54a53df6992c446307d85d75857d8ba6192b8471
QuirinoC <juanquirinoc@hotmail.com>
2017-10-09 00:27:08 -0500

    Handles more exceptions when adding contacts

--- Principal.java
+++ Principal.java
@@ -11,13 +11,15 @@ public class Principal {
         opcion=JOptionPane.showOptionDialog(null,"Elige una opción:","Bienvenido!",JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,null,opciones,opciones[0]); //Texto, Título 
     }
     
-    private static Contacto pideContacto(){
+    private static Contacto pideContacto() throws NullPointerException{
         
-        String[] datos= {"Nombre:","Apellido:","Categoría:","Correo:","Teléfono:","Celular:","Dirección:","Nota:"}; 
+        String[] datos= {"Nombre:","Apellido:","Categoría:","Correo:","Teléfono:","Celular:","Dirección:","Nota:"};
+        boolean nullFlag = true;
         for(int i=0;i<datos.length;i++){
             datos[i]=JOptionPane.showInputDialog(null,"Ingresa "+datos[i]); //Recorremos el for para mostrar las opciones y lo sobreescribimos para llamar al constructor
-            
+            if (!datos[i].equals("")) {nullFlag = false;}
         }
+        if (nullFlag) {throw new NullPointerException("");}
         Contacto c= new Contacto(datos[0],datos[1],datos[2],datos[3],datos[4],datos[5],datos[6],datos[7]);
         return c;
     }
@@ -32,7 +34,13 @@ public class Principal {
                     k=false;
                     break;
                 case 0:
-                    a.agregarContacto(pideContacto());
+                    try {
+                        a.agregarContacto(pideContacto());
+                    }
+                    catch (NullPointerException ex){
+                        JOptionPane.showMessageDialog(null,"Error al ingresar contacto","Error",JOptionPane.ERROR_MESSAGE);
+                        continue;
+                    }
                     JOptionPane.showMessageDialog(null,"Contacto agregado satisfactoriamente");
                     break;
                 case 1:
